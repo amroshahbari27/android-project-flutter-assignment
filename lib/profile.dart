a6558a18d4862d26970871d03c12a42ea0d4eff3
amroshahbari27 <64146787+amroshahbari27@users.noreply.github.com>
2021-04-18 14:21:19 +0300

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,193 +1,95 @@
 
-//------------------------ Login Page --------------------------------
-
-
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:hello_me/confirm.dart';
-class Login extends StatefulWidget {
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+class Profile extends StatefulWidget {
   final  Function(dynamic) notifyParent;
-  Login({required this.notifyParent}) ;
+  Profile(this.email,{ required this.notifyParent}) ;
+  final email ;
   @override
-  _Login createState() => _Login(this.notifyParent);
+  _Profile createState() => _Profile(email,this.notifyParent);
 }
 
-class _Login extends State<Login> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController confirmationPassword = TextEditingController();
+class _Profile extends State<Profile> {
+  bool downloadImage =true;
+  var email ;
+  File? _image;
+  NetworkImage? toShow;
   final  Function(dynamic) notifyParent;
-  bool attemptLogin = false;
-  _Login(this.notifyParent);
+  _Profile(this.email,this.notifyParent);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-          centerTitle: true,
-        ),
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Welcome to Startup Names Generator, please log in below:',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12),
-                    )),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                Container(
-                    height: 40,
-                    decoration: BoxDecoration( borderRadius: BorderRadius.all(Radius.circular(10))),
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: attemptLogin ?
-                    ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child:LinearProgressIndicator(backgroundColor: Colors.red,
-                        value: 0.8,
-                    ))
-                        : ElevatedButton(
-                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius:  new BorderRadius.circular(30),
-                                  side: BorderSide(color: Colors.red)
-                              )
-                          ) ),
-                      child: Text('Login'),
-                      onPressed: () async {
-                        //HW2
-                        try {
-                          setState(() {
-                            attemptLogin= true;
-                          });
-
-                          /*UserCredential userCredential =*/ await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: nameController.text,
-                            password: passwordController.text,
-                          );
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            print('No user found for that email.');
-                          } else if (e.code == 'wrong-password') {
-                            print('Wrong password provided for that user.');
-                          }else{
-                            print(e.toString());
-                          }
-                        }
-                          setState(() {
-                          attemptLogin= false;
-                        });
-                        User currentUser = FirebaseAuth.instance.currentUser;
-                        //if login is successful
-                        if(currentUser.uid != null) {
-                          notifyParent(nameController.text);
-                          Navigator.of(context).pop();
-                        }else{
-                          var snackBar = SnackBar(content: Text('There was an error logging into the app'));
-                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        }
-                        //needs to be changed , disable login button until firebase answers.
-                        print(nameController.text);
-                        print(passwordController.text);
-
-
-                        //HW1
-                        // snackBar = SnackBar(content: Text('Login is not implemented yet'));
-                         //ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-                      },
-                    )
-                ),
-                Container(
-                    height: 40,
-                    decoration: BoxDecoration( borderRadius: BorderRadius.all(Radius.circular(10))),
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child:  attemptLogin ? Container() : ElevatedButton(
-                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius:  new BorderRadius.circular(30),
-                                  side: BorderSide(color: Colors.green)
-                              )
-                          ) ),
-                      child: Container(child: Text('New user? Click to sign up')),
-                      onPressed: () async {
-                        showModalBottomSheet<void>(
-                          context: context,
-                          builder: (_)=>Confirm( nameController.text ,
-                               passwordController.text,notifyParent: success ),
-                        );
-                      },
-                    )
-                )
-              ],
-            )));
+      if(toShow==null )downloadPic();
+    return Container(padding: EdgeInsets.fromLTRB(2, 2, 5, 2),color:Colors.white,child:SingleChildScrollView(
+        child:Row(children: [ClipRRect(borderRadius:BorderRadius.circular(100),
+          child:
+          toShow == null ?
+          InkWell(child: CircleAvatar(radius: 50,child: Icon(Icons.add_a_photo_outlined),backgroundColor: Colors.green,),
+            onTap: (){
+              getImage();
+            },)
+              :
+          CircleAvatar(
+              radius: 50,
+              backgroundImage: toShow
+          ),),
+          Column(children: [
+            Text(' '+email,style: TextStyle(fontSize: 16),),
+            ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+                onPressed: (){
+                getImage();
+                },
+                child: Text('Change avatar',))],)],)));
   }
-  dynamic success(dynamic) async {
-  await registerUser(nameController.text, passwordController.text);
-  print('success login email : '+nameController.text);
-  }
-  Future<void> registerUser(String email,String password) async {
-    try {
-      //TODO:CHECK IF THIS IS ENOUGH
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email,
-          password: password
-      );
-      /*UserCredential userCredential =*/ await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: nameController.text,
-        password: passwordController.text,
-      );
+  //image get from gallery
+  Future<void> getImage() async {
+    PickedFile? selectedFile = await ImagePicker().getImage(source: ImageSource.gallery );
+    // ignore: unnecessary_null_comparison
+    if(selectedFile == null){
+      var snackBar = SnackBar(behavior: SnackBarBehavior.floating,content: Container(child:Text('No image selected',),));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      //show snack bar of erorr and exit .
+      return;
+    }
+    File selected = File(selectedFile.path);
+    _image=selected;
+    //TODO update database from here
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference ref = storage.ref().child(email);
+    UploadTask uploadTask = ref.putFile(_image!);
+    await uploadTask.then((res) {
+      res.ref.getDownloadURL();
+    });
+    try{
+      var url = await ref.getDownloadURL();
+      print('uploadid pic url : '+url);
       setState(() {
-        attemptLogin= false;
+        toShow=NetworkImage(url);
       });
-      User currentUser = FirebaseAuth.instance.currentUser;
-
-
-      //if login is successful
-      if(currentUser.uid != null) {
-        await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).set(Map());
-        notifyParent(nameController.text);
-        Navigator.of(context).pop();
-        Navigator.of(context).pop();
-        print(currentUser.email+ ' << email from sign up');
-      }
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
+    }catch(e){
+      print("error");
     }
   }
+ void downloadPic() async {
+
+// no need of the file extension, the name will do fine.
+
+      try{
+        final ref = FirebaseStorage.instance.ref().child(email);
+        var url ;
+        print('start');
+        url = await ref.getDownloadURL();
+        print('end');
+
+        setState(() {
+          toShow=NetworkImage(url);
+        });
+      }catch( e){
+        print('exception...........');
+       // print(e.toString());
+      }
+
+  }
+
 }
